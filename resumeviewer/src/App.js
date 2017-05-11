a9c6c1a154aaa073da1c3f2ea7ffc51179000976
eelizanjr <eelizanjr@gmail.com>
2017-05-10 23:20:44 -0400

    Added json fetch

--- resumeviewer/src/App.js
+++ resumeviewer/src/App.js
@@ -1,6 +1,7 @@
 // main Component
 
 import React, { Component } from 'react';
+import $ from 'jquery';
 import './App.css';
 // import Components
 import Header from './Components/Header';
@@ -12,11 +13,39 @@ import Resume from './Components/Resume';
 import Testimonials from './Components/Testimonials';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // put json data as the state
+      resumeData:{}
+    }
+  }
+// grab json data to put in the state
+getResumeData() {
+  $.ajax({
+    url:'http://localhost:3000/resumeData.json',
+    dataType: 'json',
+    cache: false,
+    success: function(data) {
+      this.setState({resumeData:data});
+    }.bind(this),
+    error: function(xhr, status, err) {
+      console.log(err);
+      alert(err);
+    }
+  });
+}
+
+componentDidMount(){
+  this.getResumeData();
+}
+
   render() {
+    console.log(this.state.resumeData);
     return (
       <div className="App">
       {/* must be in the order of appearance */}
-      <Header />
+      <Header data={this.state.resumeData.main} />
       <About />
       <Resume />
       <Portfolio />
